a08e1f6fd373dbfd90132aaecf7f7891c41974c6
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-09 13:54:53 +0530

    Add: program that reads 5 Random 2 Digit values then find their sum and the average

---
+++ sequence/5Random2Digits.js
@@ -0,0 +1,11 @@
+var num1 = Math.floor(Math.random() * 90) + 10;
+var num2 = Math.floor(Math.random() * 90) + 10;
+var num3 = Math.floor(Math.random() * 90) + 10;
+var num4 = Math.floor(Math.random() * 90) + 10;
+var num5 = Math.floor(Math.random() * 90) + 10;
+
+console.log("The numbers are: " + num1+","+num2+","+ num3+","+num4+","+num5);
+
+var sum = num1 + num2 + num3 + num4 + num5;
+console.log("Sum = " + sum);
+console.log("Average = " + sum/5);
