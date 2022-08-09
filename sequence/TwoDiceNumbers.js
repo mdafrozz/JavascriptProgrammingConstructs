719f6aba008b750af85c69f571e78eff9b937d49
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-09 13:53:57 +0530

    Add: Add two Random Dice Number and Print the Result

---
+++ sequence/TwoDiceNumbers.js
@@ -0,0 +1,6 @@
+var r1 = Math.floor(Math.random() * 6) + 1;
+var r2 = Math.floor(Math.random() * 6) + 1;
+console.log("Dice 1: "+r1);
+console.log("Dice 2: "+r2);
+var add = r1 + r2;
+console.log("Sum of two random dice numbers: " + add);
