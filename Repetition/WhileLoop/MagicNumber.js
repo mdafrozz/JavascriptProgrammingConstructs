7e029d9d49aa44299409e2b73a66c174e683fb6b
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-10 10:26:32 +0530

    Add: Find the Magic Number

---
+++ Repetition/WhileLoop/MagicNumber.js
@@ -0,0 +1,23 @@
+/** Find magic number */
+
+const prompts = require("prompt-sync")();
+console.log("Think of a number between 1 - 100");
+let isMagicNum = false;
+let choice;
+let upperLimit = 100;
+let lowerLimit = 1;
+while(!isNumFound){
+    choice = parseInt(prompts("Enter '1' if the number is less than "+Math.floor((lowerLimit+upperLimit)/2)+" or '2' if number is greater than "
+    +Math.floor((lowerLimit+upperLimit)/2)+" or '0' if number is equal to "+Math.floor((lowerLimit+upperLimit)/2)+" : "));
+    switch(choice){
+        case 1 : 
+            upperLimit = Math.floor((lowerLimit+upperLimit)/2);
+            break;
+        case 2 :
+            lowerLimit = Math.floor((lowerLimit+upperLimit)/2);
+            break;
+        default : 
+            console.log("Your Magic number is "+Math.floor((lowerLimit+upperLimit)/2))
+            isMagicNum = true;
+    }
+}
