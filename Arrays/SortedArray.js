65b063f1178b9fb87dffe2a24d9a1f3349edc768
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-10 10:46:52 +0530

    Add: program to sort the array and then find the 2nd largest  and the 2nd smallest element

---
+++ Arrays/SortedArray.js
@@ -0,0 +1,12 @@
+/**  sort array then find 2nd largest and 2nd smallest number */
+let random;
+let randomNum = [];
+for (let i = 0; i < 10; i++) {
+    random = Math.floor(Math.random() * 900) + 100;
+    randomNum.push(random);
+}
+randomNum.sort((a, b) => a - b);
+console.log("Sorted Array");
+console.log(randomNum);
+console.log("Second Maximum number = " + randomNum[8]);
+console.log("Second Minimum number = " + randomNum[1]);
