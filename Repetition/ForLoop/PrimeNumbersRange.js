1b7b3f61cc382c20f63b4131f8c07c1578a53246
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-10 10:21:38 +0530

    Add: program to take a range of number as input and output the Prime  Numbers in that range

---
+++ Repetition/ForLoop/PrimeNumbersRange.js
@@ -0,0 +1,17 @@
+/** Take range number of input and output the prime numbers in that range  */
+const prompts = require("prompt-sync")();
+let start = parseInt(prompts("Enter starting number of range : "));
+let end = parseInt(prompts("Enter final number of range : "));
+let isPrime;
+for (i = start; i <= end; i++) {
+    isPrime = true;
+    for (j = 2; j < i; j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(i);
+    }
+}
