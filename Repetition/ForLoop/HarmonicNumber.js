54fc2e8444fc24217a5835cfe82d0a2522c83b32
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-10 10:20:03 +0530

    Add: Program that takes a command-line argument n and prints the nth harmonic  number

---
+++ Repetition/ForLoop/HarmonicNumber.js
@@ -0,0 +1,9 @@
+/** print nth harmonic number */
+const prompts = require("prompt-sync")();
+let nthNumber = prompts("Enter the nth value : ");
+let harmonicNumber = 0;
+nthNumber = parseInt(nthNumber)
+for (i = 1; i <= nthNumber; i++) {
+    harmonicNumber += 1 / i;
+}
+console.log((i - 1) + "th Harmomnic number = " + harmonicNumber);
